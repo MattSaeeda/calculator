a8426284f972fe87f3db2f45204aca3abd7eba8d
MattSaeeda <Mattsaeeda@gmail.com>
2018-09-20 21:12:10 -0400

    calc functions

---
+++ func.js
@@ -0,0 +1,34 @@
+function add(){
+  var a = 21
+  var b =34
+  var c = a + b
+  console log(c)
+}
+
+function sub() {
+  var d = 6
+  var j = 67
+  console log(d - j)
+  
+}
+
+function div(){
+  var k = 45
+  var l = 5678
+  console log(k / l)
+}
+
+function mult(){
+  var y = 678
+  var m = 98
+  var z = "answer is"
+  console log(z + y *m)
+}
+
+function div(){
+  g = 78
+  p = 8989
+  u = g / p +1
+  l = "g divided by p is  "
+  console log(l +""+u)
+}
